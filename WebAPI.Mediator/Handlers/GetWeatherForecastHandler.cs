f1ab1c2e89a3f56baaaad80de82199619052bb57
Sebastian Przyszlak <sebastian.przyszlak@aiut.com>
2022-11-08 06:13:51 +0100

    Add WebAPI Madiator Demo Project

---
+++ WebAPI.Mediator/Handlers/GetWeatherForecastHandler.cs
@@ -0,0 +1,25 @@
+﻿using Mediator;
+using WebAPI.Mediator.Queries;
+
+namespace WebAPI.Mediator.Handlers;
+
+public class GetWeatherForecastHandler : IRequestHandler<GetWeatherForecastQuery, IEnumerable<WeatherForecast>>
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public ValueTask<IEnumerable<WeatherForecast>> Handle(GetWeatherForecastQuery request, CancellationToken cancellationToken)
+    {
+        var results = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+
+        return ValueTask.FromResult<IEnumerable<WeatherForecast>>(results);
+    }
+}
---
+++ WebAPI.Mediator/Queries/GetWeatherForecastQuery.cs
@@ -0,0 +1,8 @@
+﻿using Mediator;
+
+namespace WebAPI.Mediator.Queries;
+
+public class GetWeatherForecastQuery : IRequest<IEnumerable<WeatherForecast>>
+{
+    public static GetWeatherForecastQuery Instance = new();
+}
